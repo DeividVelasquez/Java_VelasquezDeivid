04fddfb6b73003048f2bec10601c033edd33daaf
Deivid <deividvelasquez122@gmail.com>
2024-07-29 17:00:42 -0500

    feat: :art: se cambio la conexion de mysql con java

--- Dia3/conexion/src/main/java/com/mycompany/conexion/Conexion.java
+++ Dia3/conexion/src/main/java/com/mycompany/conexion/Conexion.java
@@ -1,8 +1,8 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package conexion;
+
+package com.mycompany.conexion;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
