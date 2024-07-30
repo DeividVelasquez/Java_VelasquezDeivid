bbe674ff651519d0786c4e5a53a3620014c5e8c0
Deivid <deividvelasquez122@gmail.com>
2024-07-30 13:01:35 -0500

    feat: :sparkles: Se termino el trabajo de dia3 con efectividad

--- Dia3/Conexion/src/conexion/Conexion.java
+++ Dia3/Conexion/src/conexion/Conexion.java
@@ -1,8 +1,8 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-
-package com.mycompany.conexion;
+package conexion;
 
 import java.io.IOException;
 import java.io.InputStream;
